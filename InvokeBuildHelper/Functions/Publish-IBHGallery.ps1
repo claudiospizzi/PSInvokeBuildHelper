906cbc82591f0f23f0f542bbf304c4936bb43b5c
Claudio Spizzi <claudio@spizzi.net>
2019-10-09 01:13:22 +0200

    Update function to publish to the PSGallery

--- InvokeBuildHelper/Functions/Publish-IBHGallery.ps1
+++ InvokeBuildHelper/Functions/Publish-IBHGallery.ps1
@@ -18,8 +18,47 @@ function Publish-IBHGallery
         # Name of the module.
         [Parameter(Mandatory = $true)]
         [System.String]
-        $ModuleName
+        $ModuleName,
+
+        # Version to publish.
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $ModuleVersion,
+
+        # Gallery name.
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [System.String]
+        $GalleryUser,
+
+        # Gallery name.
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $GalleryName,
+
+        # Gallery token.
+        [Parameter(Mandatory = $true)]
+        [System.Security.SecureString]
+        $GalleryToken
     )
 
-    #
+    $releaseNotes = Get-IBHModuleReleaseNote -BuildRoot $BuildRoot -ModuleVersion $ModuleVersion
+
+    if ($GalleryToken.Length -eq 0)
+    {
+        if (-not [System.String]::IsNullOrEmpty($GalleryUser))
+        {
+            $targetName = 'IBH {0} Token ({1})' -f $GalleryName, $GalleryUser
+        }
+        else
+        {
+            $targetName = 'IBH {0} Token ({1})' -f $GalleryName, $ModuleName
+        }
+
+        $GalleryToken = Use-VaultSecureString -TargetName $targetName
+    }
+
+    $token = $GalleryToken | Unprotect-SecureString
+
+    Publish-Module -Path "$BuildRoot\$ModuleName" -Repository $GalleryName -NuGetApiKey $token -ReleaseNotes $releaseNotes
 }
