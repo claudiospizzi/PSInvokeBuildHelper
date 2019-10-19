b9729e367a0753190f6c073114a28a630b08a62a
Claudio Spizzi <claudio@spizzi.net>
2019-10-19 13:51:27 +0200

    Update the build config function

--- InvokeBuildHelper/Functions/Get-IBHConfig.ps1
+++ InvokeBuildHelper/Functions/Get-IBHConfig.ps1
@@ -3,50 +3,62 @@
         Get the build configuration.
 
     .DESCRIPTION
-        Return the build configuration object with default and calculated
-        configuration properties.
+        Return the build configuration object with default properties. Some
+        properties are calculated based on the module structure.
+
+    .OUTPUTS
+        InvokeBuildHelper.Config
+
+    .EXAMPLE
+        PS C:\> Get-IBHConfig -BuildRoot 'C:\GitHub\InvokeBuildHelper'
+        Get the build configuration.
+
+    .LINK
+        https://github.com/claudiospizzi/InvokeBuildHelper
 #>
 function Get-IBHConfig
 {
     [CmdletBinding()]
     param
     (
-        # Root path of the build script
+        # Root path of the build script.
         [Parameter(Mandatory = $true)]
         [System.String]
         $BuildRoot
     )
 
     $config = [PSCustomObject] @{
-        BuildRoot            = $BuildRoot
-        ModuleName           = Get-BuildModuleName -BuildRoot $BuildRoot
+        PSTypeName          = 'InvokeBuildHelper.Config'
 
-        VerifyTask           = [PSCustomObject] @{
-            Enabled              = $true
+        BuildRoot           = $BuildRoot
+        ModuleName          = Get-BuildModuleName -BuildRoot $BuildRoot
+
+        VerifyTask          = [PSCustomObject] @{
+            Enabled             = $true
         }
 
-        AnalyzeTask          = [PSCustomObject] @{
-            ScriptAnalyzerRules  = Get-ScriptAnalyzerRule
+        AnalyzeTask         = [PSCustomObject] @{
+            ScriptAnalyzerRules = Get-ScriptAnalyzerRule
         }
 
-        ApproveTask          = [PSCustomObject] @{
-            Enabled              = $true
-            BranchName           = 'master'
+        ApproveTask         = [PSCustomObject] @{
+            Enabled             = $true
+            BranchName          = 'master'
         }
 
-        RepositoryTask       = [PSCustomObject] @{
-            Enabled              = $true
-            Type                 = 'GitHub'
-            User                 = Get-IBHGitHubUser
-            Name                 = Get-IBHGitHubRepo
-            Token                = [System.Security.SecureString]::new()
+        RepositoryTask      = [PSCustomObject] @{
+            Enabled             = $true
+            Type                = 'GitHub'
+            User                = Get-IBHGitHubUser
+            Name                = Get-IBHGitHubRepo
+            Token               = [System.Security.SecureString]::new()
         }
 
-        GalleryTask          = [PSCustomObject] @{
-            Enabled              = $true
-            User                 = Get-IBHGitHubUser
-            Name                 = 'PSGallery'
-            Token                = [System.Security.SecureString]::new()
+        GalleryTask         = [PSCustomObject] @{
+            Enabled             = $true
+            User                = Get-IBHGitHubUser
+            Name                = 'PSGallery'
+            Token               = [System.Security.SecureString]::new()
         }
     }
 
