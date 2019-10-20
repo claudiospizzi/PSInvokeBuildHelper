a562a52566a8d7ee8b28e7962b943f5d18200c1b
Claudio Spizzi <claudio@spizzi.net>
2019-10-20 21:17:08 +0200

    Add example, reorder parameters

--- InvokeBuildHelper/Functions/Publish-IBHGallery.ps1
+++ InvokeBuildHelper/Functions/Publish-IBHGallery.ps1
@@ -5,6 +5,16 @@
     .DESCRIPTION
         Use the build-in Publish-Module command to publish a new module version
         including the release notes.
+
+    .OUTPUTS
+        None. No output if successful or an exception in case of an error.
+
+    .EXAMPLE
+        PS C:\> Publish-IBHGallery -BuildRoot 'C:\GitHub\InvokeBuildHelper' -ModuleName 'InvokeBuildHelper' -ModuleVersion '1.0.0' -GalleryName 'PSGallery' -GalleryUser $user -GalleryToken $token
+        Publish the module InvokeBuildHelper version 1.0.0 to the PSGallery.
+
+    .LINK
+        https://github.com/claudiospizzi/InvokeBuildHelper
 #>
 function Publish-IBHGallery
 {
@@ -28,14 +38,14 @@ function Publish-IBHGallery
 
         # Gallery name.
         [Parameter(Mandatory = $true)]
+        [System.String]
+        $GalleryName,
+
+        # Gallery user.
+        [Parameter(Mandatory = $true)]
         [AllowEmptyString()]
         [System.String]
         $GalleryUser,
-
-        # Gallery name.
-        [Parameter(Mandatory = $true)]
-        [System.String]
-        $GalleryName,
 
         # Gallery token.
         [Parameter(Mandatory = $true)]
