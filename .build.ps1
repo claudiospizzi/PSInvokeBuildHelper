759112601e613b23cccb00bbe29e9079af8fed45
Claudio Spizzi <claudio@spizzi.net>
2019-10-22 17:50:32 +0200

    Update credentials in build script

--- .build.ps1
+++ .build.ps1
@@ -7,4 +7,6 @@ Import-Module .\InvokeBuildHelper\InvokeBuildHelper.psd1
 . InvokeBuildHelperTasks
 
 # Build configuration
-$IBHConfig.VerifyTask.Enabled = $false
+$IBHConfig.VerifyTask.Enabled   = $false
+$IBHConfig.RepositoryTask.Token = Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)'
+$IBHConfig.GalleryTask.Token    = Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)'
