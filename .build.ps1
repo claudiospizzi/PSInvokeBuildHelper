5571beb58737ca2e7707d84a01271c0574e8c3cf
Claudio Spizzi <claudio@spizzi.net>
2019-10-23 09:57:01 +0200

    Add note to the build script

--- .build.ps1
+++ .build.ps1
@@ -1,4 +1,17 @@
 
+# Build information
+Write-Host -ForegroundColor 'DarkYellow' -Object ''
+Write-Host -ForegroundColor 'DarkYellow' -Object 'IMPORTANT NOTE'
+Write-Host -ForegroundColor 'DarkYellow' -Object '**************'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'You are currently using the build helper tasks of the InvokeBuildHelper module'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'against itself. There are some limitations in this case: The Release task is'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'not supported. Please use the following sub-tasks:'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'PS C:\> Invoke-Build'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'PS C:\> Invoke-Build -Task "Approve"'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'PS C:\> Invoke-Build -Task "Gallery"'
+Write-Host -ForegroundColor 'DarkYellow' -Object 'PS C:\> Invoke-Build -Task "Repository"'
+Write-Host -ForegroundColor 'DarkYellow' -Object ''
+
 # Import module
 Get-Module InvokeBuildHelper | Remove-Module
 Import-Module .\InvokeBuildHelper\InvokeBuildHelper.psd1
