6b4cbef8b05b09a8584adc7eeb3f8978dff2b921
Claudio Spizzi <claudio@spizzi.net>
2020-06-02 09:16:39 +0200

    Move argument completer to function script

--- InvokeBuildHelper/Functions/Invoke-BuildIsolated.ps1
+++ InvokeBuildHelper/Functions/Invoke-BuildIsolated.ps1
@@ -37,3 +37,23 @@ function Invoke-BuildIsolated
     $powershellCommand = Get-Command -Name 'powershell.exe' | Select-Object -ExpandProperty 'Source'
     & $powershellCommand -NoLogo -NoProfile -ExecutionPolicy 'Bypass' -Command "Set-Location -Path '$($pwd.ProviderPath)'; Invoke-Build -Task '$Task'"
 }
+
+# Register the argument completer for the Invoke-Build command
+Register-ArgumentCompleter -CommandName 'Invoke-Build.ps1', 'Invoke-BuildIsolated' -ParameterName 'Task' -ScriptBlock {
+    param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    if (Test-Path -Path '.\.build.ps1')
+    {
+        if (Select-String -Path '.\.build.ps1' -Pattern '\. InvokeBuildHelperTasks' -Quiet)
+        {
+            $match = Select-String -Path "$PSScriptRoot\Scripts\InvokeBuildHelperTasks.ps1" -Pattern '^task (?<taskname>[a-zA-Z]+)'
+            foreach ($capture in $match.Matches.Captures)
+            {
+                $taskName = $capture.Groups[1].Value
+                if ($taskName -like "$wordToComplete*")
+                {
+                    [System.Management.Automation.CompletionResult]::new($taskName, $taskName, 'ParameterValue', $taskName)
+                }
+            }
+        }
+    }
+}
